c9ce554d78453d85ffb82bd3629d79fb28db24e1
Unknown <dimidyuk9897@gmail.com>
2018-07-31 14:03:04 +0300

    Refactored

---
+++ No7.Solution/TradeCreator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace No7.Solution
+{
+    //Класс для создания трейдов
+    public class TradeCreator
+    {
+        public static List<TradeRecord> CreateTradeList(List<string> lines)
+        {
+            var trades = new List<TradeRecord>();
+
+            int lineNumber = 0;
+
+            foreach (var line in lines)
+            {
+                lineNumber++;
+                var field = line.Split(new char[] { ',' });
+
+                //проверка на заполнение полей
+                if(!CheckFields(field, lineNumber))
+                {
+                    continue;
+                }
+
+                //Не придумал как это обыграть
+                if (!int.TryParse(field[1], out var tradeAmount))
+                {
+                    Console.WriteLine($"WARN: Trade amount on line {lineNumber} not a valid integer: '{field[1]}'");
+                }
+
+                if (!decimal.TryParse(field[2], out var tradePrice))
+                {
+                    Console.WriteLine($"WARN: Trade price on line {lineNumber} not a valid decimal: '{field[2]}'");
+                }
+
+                TradeRecord trade = new TradeRecord
+                {
+                    SourceCurrency = field[0].Substring(0, 3),
+                    DestinationCurrency = field[0].Substring(3, 3),
+                    Lots = tradeAmount / 100000f,
+                    Price = tradePrice
+                };
+
+                trades.Add(trade);
+            }
+            return trades;
+        }
+
+        //Проверка полей. Объяснение ошибок
+        private static bool CheckFields(string[] fields, int lineNumber)
+        {
+            if (fields.Length != 3)
+            {
+                Console.WriteLine($"WARN: Line {lineNumber} malformed. Only {fields.Length} field(s) found.");
+                return false;
+            }
+
+            if (fields[0].Length != 6)
+            {
+                Console.WriteLine($"WARN: Trade currencies on line {lineNumber} malformed: {fields[0]} is not suitable.");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ No7.Solution/TradeHandler.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Globalization;
+
+namespace No7.Solution
+{
+
+    public class TradeHandler
+    {
+        public void HandleTrades(Stream stream)
+        {
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+
+            var lines = new List<string>();
+            using (var reader = new StreamReader(stream))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+            }
+
+            var trades = TradeCreator.CreateTradeList(lines);
+
+            SaveInDataBase(trades);
+        }
+
+        private void SaveInDataBase(List<TradeRecord> trades)
+        {
+            // save into database
+            string connectionString = ConfigurationManager.ConnectionStrings["TradeData"].ConnectionString;
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (var transaction = connection.BeginTransaction())
+                {
+                    foreach (var trade in trades)
+                    {
+                        var command = connection.CreateCommand();
+                        command.Transaction = transaction;
+                        command.CommandType = System.Data.CommandType.StoredProcedure;
+                        command.CommandText = "dbo.Insert_Trade";
+                        command.Parameters.AddWithValue("@sourceCurrency", trade.SourceCurrency);
+                        command.Parameters.AddWithValue("@destinationCurrency", trade.DestinationCurrency);
+                        command.Parameters.AddWithValue("@lots", trade.Lots);
+                        command.Parameters.AddWithValue("@price", trade.Price);
+
+                        command.ExecuteNonQuery();
+                    }
+
+                    transaction.Commit();
+                }
+                connection.Close();
+            }
+        }
+    }
+}
